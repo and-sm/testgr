e5689487eb41ed98de3799308f14679f95a8b8c3
Andrey Smirnov <and.inbx@gmail.com>
2018-10-13 15:37:11 +0300

    0.6.0 management

---
+++ management/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^management$', views.main, name='management'),
+    url(r'^api/env/change$', views.api_remap_env, name='api_remap_env'),
+    url(r'^api/env/delete$', views.api_delete_env, name='api_delete_env')
+]
