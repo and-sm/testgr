56f0a62d183123a68f8713bcde858eed39eb238e
Andrey Smirnov <and.inbx@gmail.com>
2020-08-03 15:37:32 +0300

    1.4.0 fix TESTGR_URL

--- testgr/settings.py
+++ testgr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # HTTPS settings
-if "https://" in os.getenv('TESTGR_URL'):
+if os.getenv('TESTGR_URL') and "https://" in os.getenv('TESTGR_URL'):
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
