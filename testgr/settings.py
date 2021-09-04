e218522a7e0112d3029be1d3c6d9affd1a999be7
Andrey Smirnov <and.inbx@gmail.com>
2021-09-05 00:42:53 +0300

    Fix app

--- testgr/settings.py
+++ testgr/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'anymail',
     'management',
     'channels',
