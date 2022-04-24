76fbe195bbfd33cd8666aca963f408d73a95cbfb
Andrey Smirnov <and.inbx@gmail.com>
2022-04-24 13:04:35 +0300

    1.14.0 users/edit fix

--- management/urls.py
+++ management/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     re_path(r'^management/about$', views.about, name='about'),
     re_path(r'^management/users$', views.users, name='users'),
     re_path(r'^management/users/add$', views.users_add, name='users_add'),
-    re_path('management/users/edit/<pk>/', views.users_edit, name='users_edit'),
+    path('management/users/edit/<pk>/', views.users_edit, name='users_edit'),
     re_path(r'^management/settings$', views.settings, name='settings'),
 ]
